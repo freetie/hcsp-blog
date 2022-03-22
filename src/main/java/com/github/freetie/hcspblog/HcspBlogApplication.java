9794a365c1606d8683df21c408e2d02687484303
wb-zt570515 <wb-zt570515@antgroup.com>
2022-03-22 15:04:11 +0800

    Initialize Spring Boot

---
+++ src/main/java/com/github/freetie/hcspblog/HcspBlogApplication.java
@@ -0,0 +1,13 @@
+package com.github.freetie.hcspblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcspBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcspBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/freetie/hcspblog/HcspBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.freetie.hcspblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcspBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
