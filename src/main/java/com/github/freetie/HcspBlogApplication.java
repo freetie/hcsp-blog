b34d77d79196a9c3c223f22f7fe03fe6927410e1
wb-zt570515 <wb-zt570515@antgroup.com>
2022-03-22 15:04:11 +0800

    Initialize Spring Boot

---
+++ src/main/java/com/github/freetie/HcspBlogApplication.java
@@ -0,0 +1,13 @@
+package com.github.freetie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcspBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcspBlogApplication.class, args);
+	}
+
+}
